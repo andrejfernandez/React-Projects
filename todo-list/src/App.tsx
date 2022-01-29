367687c07848f40ccd63204d723217b8ced5b15e
Andre <andrejfernandez@protonmail.com>
2022-01-29 12:46:22 -0500

    Updated ReadMe

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App bg-rose-800">
       <header className="App-header bg-rose-800">
-        <h1 className="bg-slate-400 bg-rose-800">Hello World!</h1>
+        <h1 className="bg-slate-400 bg-rose-800">Hello World!!</h1>
       </header>
     </div>
   );
